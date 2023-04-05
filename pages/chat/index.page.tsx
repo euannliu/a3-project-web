8f8b548669139ca802d0488d7eb7a1828952c6f6
Eu-Ann Liu <euannliu@gmail.com>
2023-04-05 00:37:27 -0700

    navbar

---
+++ pages/chat/index.page.tsx
@@ -0,0 +1,11 @@
+import {
+  TextField 
+} from '@mui/material'
+
+export default function Chat() {
+  return (
+    <>
+      <TextField label="You can also type to chat..." variant="standard"/>
+    </>
+  )
+}
